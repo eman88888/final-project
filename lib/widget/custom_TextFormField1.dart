12c4a755df3a9b82dd8634b06c0d32f3a388f91e
Your Name <hagaryusuf@gmail.com>
2024-04-30 22:08:45 +0300

    commit two

--- lib/widget/custom_TextFormField1.dart
+++ lib/widget/custom_TextFormField1.dart
@@ -1,8 +1,8 @@
+import 'dart:html';
 
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatefulWidget {
-
   CustomTextFormField({
     required this.iconData,
     required this.controller,
@@ -10,7 +10,8 @@ class CustomTextFormField extends StatefulWidget {
     required this.labelFontSize,
     required this.labelColor,
     required this.labelFontWeight,
-    
+    required this.hintText, 
+   
   });
   final IconData iconData;
   final TextEditingController controller;
@@ -18,52 +19,49 @@ class CustomTextFormField extends StatefulWidget {
   final double labelFontSize;
   final Color labelColor;
   final labelFontWeight;
+   final String hintText;
 
-  
   @override
   State<CustomTextFormField> createState() => _CustomTextFormFieldState();
 }
 
 class _CustomTextFormFieldState extends State<CustomTextFormField> {
-  
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: TextFormField(
-                        controller: widget.controller,
-                        decoration: InputDecoration(
-                          suffixIcon: Icon(widget.iconData,color: Color(0xFF1D5D9B)),
-                          filled: true,
-                          fillColor: Color.fromARGB(255, 228, 233, 251),
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-                          contentPadding:
-                              EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-                              labelText: (widget.labelText),
-                               labelStyle: TextStyle(
-                                   fontSize: widget.labelFontSize, 
-                                   color: widget.labelColor,
-                                   fontWeight: widget.labelFontWeight, 
-                                       ),
-                           focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: BorderSide(
-                              color: Color(0xFF047EB0),
-                            ),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: BorderSide(
-                              color: Color(0xFF88AACA),
-                            ),
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color(0xFF88AACA),
-                            ),
-                          )
-                        ),
+        controller: widget.controller,
+        decoration: InputDecoration(
+            suffixIcon: Icon(widget.iconData, color: Color(0xFF1D5D9B)),
+            filled: true,
+            fillColor: Color.fromARGB(255, 228, 233, 251),
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            contentPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+            labelText: (widget.labelText),
+            hintText: widget.hintText,
+            labelStyle: TextStyle(
+              fontSize: widget.labelFontSize,
+              color: widget.labelColor,
+              fontWeight: widget.labelFontWeight,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(
+                color: Color(0xFF047EB0),
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(
+                color: Color(0xFF88AACA),
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color(0xFF88AACA),
+              ),
+            )),
       ),
     );
   }
